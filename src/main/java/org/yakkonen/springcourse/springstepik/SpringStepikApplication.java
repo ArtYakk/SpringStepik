53c4937cf9fba6ea47d2294e836c715d66ee79b3
artem <iakonien@gmail.com>
2024-08-21 06:42:51 +0300

    frst

---
+++ src/main/java/org/yakkonen/springcourse/springstepik/SpringStepikApplication.java
@@ -0,0 +1,13 @@
+package org.yakkonen.springcourse.springstepik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStepikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStepikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yakkonen/springcourse/springstepik/SpringStepikApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yakkonen.springcourse.springstepik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStepikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
