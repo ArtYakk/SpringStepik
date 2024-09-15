5e6866024f762989bfcea8170a4da5bf4b1f13ac
artem <iakonien@gmail.com>
2024-09-15 13:25:32 +0300

    init() and destroy()

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,18 +1,25 @@
 package spring_introduction;
 
 public class Dog implements Pet {
-    private String name;
+//    private String name;
 
     public Dog(){
         System.out.println("Dog bean is created");
     }
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
-    public void setName(String name) {
-        this.name = name;
+    public void init(){
+        System.out.println("Class dog: init method called");
+    }
+    public void destroy(){
+        System.out.println("Class dog: destroy method called");
     }
 
     @Override
--- src/main/java/spring_introduction/Test4.java
+++ src/main/java/spring_introduction/Test4.java
@@ -6,16 +6,16 @@ public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
-        Dog myDog = (Dog) context.getBean("myDog", Dog.class);
-        myDog.setName("Belka");
-        Dog yourDog = (Dog) context.getBean("myDog", Dog.class);
-        yourDog.setName("Strelka");
-
-
-        System.out.println("Переменные ссылаются на один и тот же объект? "
-        + (myDog==yourDog));
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
+//        Dog myDog = (Dog) context.getBean("myDog", Dog.class);
+//        myDog.setName("Belka");
+//        Dog yourDog = (Dog) context.getBean("myDog", Dog.class);
+//        yourDog.setName("Strelka");
+//
+//
+//        System.out.println("Переменные ссылаются на один и тот же объект? "
+//        + (myDog==yourDog));
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
 
 
 
