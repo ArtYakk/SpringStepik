990e6703fb8fa96d6ac43ad7d4d1d50b76ab86fe
artem <iakonien@gmail.com>
2024-10-31 16:18:12 +0300

    Отношение один ко многим (Bi-directional)

--- src/main/java/hibernate_test_one_to_one/Test2.java
+++ src/main/java/hibernate_test_one_to_one/Test2.java
@@ -1,12 +1,12 @@
-package hibernate_test_2;
+package hibernate_test_one_to_one;
 
-import hibernate_test_2.entity.Detail;
-import hibernate_test_2.entity.Employee;
+import hibernate_test_one_to_one.entity.Detail;
+import hibernate_test_one_to_one.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args)  {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -17,14 +17,22 @@ public class Test1 {
         Session session = null;
 
         try {
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//            Detail detail = session.get(Detail.class, 4);
+//            System.out.println(detail.getEmployee());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
             session = factory.getCurrentSession();
 
-
             session.beginTransaction();
-            Employee emp = session.get(Employee.class, 2);
-           session.delete(emp);
+            Detail detail = session.get(Detail.class, 1);
+            detail.getEmployee().setEmpDetail(null);
 
-
+            session.delete(detail) ;
             session.getTransaction().commit();
             System.out.println("Done!");
 
